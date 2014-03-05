3929724cc4717aed02e491751e6ba6e3c626f1d6
István Gazsi <istvan.gazsi@theag3nt.com>
2014-03-05 15:59:44 +0100

    Added license boilerplate and fixed flake8 errors.

--- shell.py
+++ shell.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 
-import sys, subprocess
+# shell.py - Python wrapper Popen.
+# Copyright (C) 2014 István Gazsi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import subprocess
+
 
 class Shell(object):
-
     """
     Wrapper object around Popen.
 
     Usage:
 
-        >>> sh = Shell()
-        >>> sh(command, *args, **kwargs)    ->  <command> <kwargs> <args>
-        >>> sh.command(*args, **kwargs)     ->  <command> <kwargs> <args>
+        sh = Shell()
+        sh("command", *args, **kwargs)    # ->  <command> <kwargs> <args>
+        sh.command(*args, **kwargs)     # ->  <command> <kwargs> <args>
 
     Examples:
 
@@ -58,14 +75,14 @@ class Shell(object):
             __foo_bar="baz"  ->  "--foo-bar baz"
               foo_bar="baz"  ->  "foo_bar baz"
     """
-    
-    BOOL    = 0
-    OUTPUT  = 1
-    POPEN   = 2
-    
+
+    BOOL = 0
+    OUTPUT = 1
+    POPEN = 2
+
     def __init__(self, return_type=POPEN):
         self.return_type = return_type
-    
+
     def __call__(self, command, *args, **kwargs):
         command_line = [command]
         for key, value in kwargs.iteritems():
@@ -80,18 +97,26 @@ class Shell(object):
         else:
             command_line = command_line[0]
         if self.return_type == Shell.BOOL:
-            return subprocess.call(command_line, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=(sys.platform=="win32")) == 0
+            return subprocess.call(command_line, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   shell=(sys.platform == "win32")) == 0
         elif self.return_type == Shell.OUTPUT:
             try:
-                return subprocess.check_output(command_line, stderr=subprocess.STDOUT, shell=(sys.platform=="win32"))
+                return subprocess.check_output(command_line,
+                                               stderr=subprocess.STDOUT,
+                                               shell=(sys.platform == "win32"))
             except subprocess.CalledProcessError as e:
                 return e.output
         else:
-            return subprocess.Popen(command_line, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=(sys.platform=="win32"))
-    
+            return subprocess.Popen(command_line, stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    shell=(sys.platform == "win32"))
+
     def __getattr__(self, command):
         def wrapper(*args, **kwargs):
             if command.startswith("_"):
-                return self(command.lstrip("_").replace("_", "-"), *args, **kwargs)
+                return self(command.lstrip("_").replace("_", "-"),
+                            *args, **kwargs)
             return self(command, *args, **kwargs)
         return wrapper
